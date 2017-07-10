346f45e39d160919a0fb682c8db524cb2766c71c
Nikita Nazarov <nikita.nazarov@unilead.net>
2017-07-10 16:25:38 +0300

    Add AdvertiserUser class to contain namespace.

---
+++ src/Contain/AdvertiserUser.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Unilead | HasOffers
+ *
+ * This file is part of the Unilead Service Package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package     HasOffers
+ * @license     Proprietary
+ * @copyright   Copyright (C) Unilead Network, All rights reserved.
+ * @link        https://www.unileadnetwork.com
+ */
+
+namespace Unilead\HasOffers\Contain;
+
+use JBZoo\Data\Data;
+use Unilead\HasOffers\Entity\Advertiser;
+use Unilead\HasOffers\Traits\Data as DataTrait;
+
+/**
+ * Class AdvertiserUser
+ * @package Unilead\HasOffers
+ */
+class AdvertiserUser
+{
+    use DataTrait;
+
+    /**
+     * @var Advertiser
+     */
+    protected $advertiser;
+
+    /**
+     * @var Data
+     */
+    protected $users;
+
+    /**
+     * AdvertiserUser constructor.
+     *
+     * @param array      $data
+     * @param Advertiser $advertiser
+     */
+    public function __construct(array $data, Advertiser $advertiser)
+    {
+        $this->advertiser = $advertiser;
+        $this->users = $data;
+    }
+
+    /**
+     * @return Data
+     */
+    public function getRawData()
+    {
+        return $this->users;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsersList()
+    {
+        $data = array_reduce($this->users, function ($reduced, $current) {
+            $current = $this->cutKeys($current);
+
+            $reduced[$current['id']] = $current;
+
+            return $reduced;
+        });
+
+        return $data;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function reload()
+    {
+        $data = $this->getRawData()->getArrayCopy();
+
+        $this->advertiser->getClient()->trigger('advertiser_users.reload.before', [$this, &$data]);
+
+        $this->bindData($data);
+
+        $this->advertiser->getClient()->trigger('advertiser_users.reload.after', [$this, $data]);
+    }
+
+    /**
+     * @param array $array
+     *
+     * @return mixed
+     */
+    private function cutKeys($array)
+    {
+        $removeKeys = [
+            'wants_alerts',
+            'SHARED_Users2_id',
+            'salt',
+            'AFFILIATE_NETWORK_Brands_id',
+            '_NETWORK_employees_id',
+            'access',
+        ];
+
+        foreach ($removeKeys as $removeKey) {
+            unset($array[$removeKey]);
+        }
+
+        return $array;
+    }
+}
