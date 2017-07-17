b3f2eb0bff2ca236be92a82733da1e1a4a18ad46
Denis Smetannikov <denis.smetannikov@unilead.net>
2017-07-17 18:53:41 +0300

    add autocomplete

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Unilead | BM
+ * Unilead | HasOffers
  *
  * This file is part of the Unilead Service Package.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @package     BM
+ * @package     HasOffers
  * @license     Proprietary
  * @copyright   Copyright (C) Unilead Network, All rights reserved.
  * @link        https://www.unileadnetwork.com
