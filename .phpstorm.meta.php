d6d926066240659032da110239227aa3ef0931e3
Denis Smetannikov <denis.smetannikov@unilead.net>
2017-07-17 19:09:26 +0300

    add autocomplete

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -18,29 +18,58 @@
 
 namespace PHPSTORM_META {
 
-    use \Unilead\HasOffers\HasOffersClient;
-    use \Unilead\HasOffers\Entity\Advertiser;
-    use \Unilead\HasOffers\Entity\AdvertiserInvoice;
-    use \Unilead\HasOffers\Entity\AdvertiserInvoiceItem;
-    use \Unilead\HasOffers\Entity\AdvertiserUser;
-    use \Unilead\HasOffers\Entity\Affiliate;
-    use \Unilead\HasOffers\Entity\AffiliateInvoice;
-    use \Unilead\HasOffers\Entity\AffiliateInvoiceItem;
-    use \Unilead\HasOffers\Entity\AffiliateUser;
-    use \Unilead\HasOffers\Entity\Employee;
-    use \Unilead\HasOffers\Offer;
+    use Unilead\HasOffers\HasOffersClient;
+    use Unilead\HasOffers\Entity\Advertiser;
+    use Unilead\HasOffers\Entity\AdvertiserInvoice;
+    use Unilead\HasOffers\Entity\AdvertiserInvoiceItem;
+    use Unilead\HasOffers\Entity\AdvertiserUser;
+    use Unilead\HasOffers\Entity\Affiliate;
+    use Unilead\HasOffers\Entity\AffiliateInvoice;
+    use Unilead\HasOffers\Entity\AffiliateInvoiceItem;
+    use Unilead\HasOffers\Entity\AffiliateUser;
+    use Unilead\HasOffers\Entity\Employee;
+    use Unilead\HasOffers\Entity\Offer;
 
     override(HasOffersClient::get(0),
         map([
-            Advertiser::class            => Advertiser::class,
-            AdvertiserInvoice::class     => AdvertiserInvoice::class,
-            AdvertiserInvoiceItem::class => AdvertiserInvoiceItem::class,
-            AdvertiserUser::class        => AdvertiserUser::class,
-            Affiliate::class             => Affiliate::class,
-            AffiliateInvoice::class      => AffiliateInvoice::class,
-            AffiliateInvoiceItem::class  => AffiliateInvoiceItem::class,
-            AffiliateUser::class         => AffiliateUser::class,
-            Employee::class              => Employee::class,
-            Offer::class                 => Offer::class,
+            Advertiser::class                      => Advertiser::class,
+            'Advertiser'                           => Advertiser::class,
+            '\Unilead\HasOffers\Entity\Advertiser' => Advertiser::class,
+
+            AdvertiserInvoice::class                      => AdvertiserInvoice::class,
+            'AdvertiserInvoice'                           => AdvertiserInvoice::class,
+            '\Unilead\HasOffers\Entity\AdvertiserInvoice' => AdvertiserInvoice::class,
+
+            AdvertiserInvoiceItem::class                      => AdvertiserInvoiceItem::class,
+            'AdvertiserInvoiceItem'                           => AdvertiserInvoiceItem::class,
+            '\Unilead\HasOffers\Entity\AdvertiserInvoiceItem' => AdvertiserInvoiceItem::class,
+
+            AdvertiserUser::class                      => AdvertiserUser::class,
+            'AdvertiserUser'                           => AdvertiserUser::class,
+            '\Unilead\HasOffers\Entity\AdvertiserUser' => AdvertiserUser::class,
+
+            Affiliate::class                      => Affiliate::class,
+            'Affiliate'                           => Affiliate::class,
+            '\Unilead\HasOffers\Entity\Affiliate' => Affiliate::class,
+
+            AffiliateInvoice::class                      => AffiliateInvoice::class,
+            'AffiliateInvoice'                           => AffiliateInvoice::class,
+            '\Unilead\HasOffers\Entity\AffiliateInvoice' => AffiliateInvoice::class,
+
+            AffiliateInvoiceItem::class                      => AffiliateInvoiceItem::class,
+            'AffiliateInvoiceItem'                           => AffiliateInvoiceItem::class,
+            '\Unilead\HasOffers\Entity\AffiliateInvoiceItem' => AffiliateInvoiceItem::class,
+
+            AffiliateUser::class                      => AffiliateUser::class,
+            'AffiliateUser'                           => AffiliateUser::class,
+            '\Unilead\HasOffers\Entity\AffiliateUser' => AffiliateUser::class,
+
+            Employee::class                      => Employee::class,
+            'Employee'                           => Employee::class,
+            '\Unilead\HasOffers\Entity\Employee' => Employee::class,
+
+            Offer::class                      => Offer::class,
+            'Offer'                           => Offer::class,
+            '\Unilead\HasOffers\Entity\Offer' => Offer::class,
         ]));
 }
