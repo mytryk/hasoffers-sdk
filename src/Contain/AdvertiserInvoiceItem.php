07ce53b157dba0fb8350c7a032a60b396ba24444
Dmitriy Savin <savin_dmitriy.91@mail.ru>
2017-08-24 11:55:41 +0300

    Fix description

--- src/Contain/AdvertiserInvoiceItem.php
+++ src/Contain/AdvertiserInvoiceItem.php
@@ -20,33 +20,34 @@ use Unilead\HasOffers\Entity\AdvertiserInvoice;
  * Class AdvertiserInvoiceItem
  *
  * @property string actions         Integer    The contents of this field are applicable only if "type" is "stats".
- *           Count of the number of clicks, conversions, or impressions, for the date covered by the Invoice start and
- *           end dates. The type of event contained in this column depends on the "payout_type". For "cpc" payout type
- *           this is number of clicks. For "cpm" this is number of impressions. For other payout types this is number
- *           of conversions.
+ *                                  Count of the number of clicks, conversions, or impressions, for the date covered
+ *                                  by the Invoice start and end dates.
+ *                                  The type of event contained in this column depends on the "payout_type". For "cpc"
+ *                                  payout type this is number of clicks. For "cpm" this is number of impressions. For
+ *                                  other payout types this is number of conversions.
  * @property string amount          Decimal    Amount owed for the offer based on invoice start and end dates
  * @property string conversions     Integer    DEPRECATED. The "actions" field should be consulted instead, to
- *           determine the number of clicks/conversions/impressions (if "type" is "stats").
+ *                                  determine the number of clicks/conversions/impressions (if "type" is "stats").
  * @property string datetime        Datetime    Date that the invoice item was created
  * @property string goal_id         Nullable Integer    Applicable only if the "enable_offer_goals" Preference is
- *           enabled. It specifies the Goal this invoice item is attributed, or NULL if the item is not attributed to a
- *           Goal.
+ *                                  enabled. It specifies the Goal this invoice item is attributed, or NULL if the
+ *                                  item is not attributed to a Goal.
  * @property string id              Integer    ID of unique, auto-generated object for this Invoice Item
  * @property string invoice_id      Integer    The ID of the Invoice to which this Invoice Item belongs
  * @property string memo            Nullable String    Memo for this Invoice Item
  * @property string offer_id        Nullable Integer    The ID of the Offer to which this item is attributed. Present
- *           when the "type" is "stats".
+ *                                  when the "type" is "stats".
  * @property string revenue_type    String    The revenue type of the Offer (or the Goal if present) if the "type" is
- *           "stats", or the adjustment type if the "type" is "adjustment". For types other than "stats" set to
- *           "amount"
+ *                                  "stats", or the adjustment type if the "type" is "adjustment". For types other than
+ *                                  "stats" set to "amount"
  * @property string type            String    Type of invoice item. "stats" indicates this is for
- *           clicks/conversions/impressions (depending on "revenue_type"). "adjustment" is for an adjustment. "vat" is
- *           for VAT/tax items.
+ *                                  clicks/conversions/impressions (depending on "revenue_type"). "adjustment" is for
+ *                                  an adjustment. "vat" is for VAT/tax items.
  * @property string vat_code        Nullable String    The VAT code. Only applicable if "type" is "vat".
  * @property string vat_id          Nullable Integer    The ID of this VAT rate. Only applicable if "type" is "vat".
  * @property string vat_name        Nullable String    The name of this VAT rate. Only applicable if "type" is "vat".
  * @property string vat_rate        Nullable Decimal    The VAT rate being used to calculate tax, as a percentage (e.g.
- *           15.00). Only applicable if "type" is "vat".
+ *                                  15.00). Only applicable if "type" is "vat".
  *
  * @package Unilead\HasOffers
  */
