71f6323f42313dadfe828e210ef85f773ae69506
Dmitriy Savin <savin_dmitriy.91@mail.ru>
2018-03-06 22:13:11 +0300

    Added vat rate model

---
+++ src/Entity/VatRate.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Unilead | HasOffers
+ *
+ * This file is part of the Unilead Service Package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package     HasOffers
+ * @license     Proprietary
+ * @copyright   Copyright (C) Unilead Network, All rights reserved.
+ * @link        https://www.unileadnetwork.com
+ */
+
+namespace Unilead\HasOffers\Entity;
+
+use Unilead\HasOffers\Traits\Deleted;
+
+/* @noinspection ClassOverridesFieldOfSuperClassInspection */
+
+/**
+ * Class VatRate
+ *
+ * @property string $id                 A unique, auto-generated ID for the VatRate
+ * @property string $code               The code for this VAT Rate
+ * @property \Datetime $created         The date this VAT Rate was created
+ * @property \Datetime $modified        The last time this VAT Rate was modified
+ * @property string $name               The name given to this VAT Rate
+ * @property float $rate                The percentage of the VAT
+ *
+ * @package Unilead\HasOffers\Entity
+ */
+class VatRate extends AbstractEntity
+{
+    use Deleted;
+
+    /**
+     * @var string
+     */
+    protected $target = 'VatRate';
+
+    /**
+     * @var array
+     */
+    protected $methods = [
+        'get' => 'findById',
+        'create' => 'create',
+        'update' => 'update',
+    ];
+}
